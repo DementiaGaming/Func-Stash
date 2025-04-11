62b1c2fa87b354a07e1bf310228683ce1a251849
DementiaGaming <sukhrathansidhu@outlook.com>
2025-04-11 10:21:12 +0100

    change py++ to pypp

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="py++",
+    name="pypp",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[],  # Add dependencies here
